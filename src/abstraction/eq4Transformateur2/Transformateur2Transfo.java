b19b10008e57b339c4737a8dfb70ef28c67693d8
gabyt <gabyt@172.16.125.246>
2022-04-13 12:21:40 +0200

    Premier pas

--- src/abstraction/eq4Transformateur2/Transformateur2Transfo.java
+++ src/abstraction/eq4Transformateur2/Transformateur2Transfo.java
@@ -1,5 +1,5 @@
 package abstraction.eq4Transformateur2;
 
-public class Transformateur2Transfo extends Transformateur2Achat {
+public class Transformateur2Transfo extends Transformateur2Vente {
 
 }
