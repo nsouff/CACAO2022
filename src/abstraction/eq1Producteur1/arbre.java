5d867dc32ed2dd835f450e466a3a6709c9ed5e70
maxim <maxim@172.16.122.162>
2022-04-13 10:46:01 +0200

    Merge branch 'main' of https://github.com/Keracles/CACAO2022 into main

---
+++ src/abstraction/eq1Producteur1/arbre.java
@@ -0,0 +1,29 @@
+package abstraction.eq1Producteur1;
+
+public class arbre {
+	private int ut_plantation;
+	private int ut_esperance_vie;
+	private int maladie;
+	private int qualité;
+	private boolean bioéquitable;
+	private boolean transition_bio;
+	private int stade_transition;
+	
+	
+	public void set_ut_plantation(int ut_plantation) {
+		this.ut_plantation = ut_plantation;
+	}
+	
+	public void 
+	public void setMaladie(int maladie) {
+		this.maladie=maladie;
+	}
+	public void setQualité() {
+		this.qualité=qualité;
+	
+	}
+	public void setBioéquitable() {
+		this.bioéquitable= 
+	}
+
+}
