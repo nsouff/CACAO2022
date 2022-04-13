de0290a38ae248efc33e76589456303a44bcef78
emmah78 <103105820+emmah78@users.noreply.github.com>
2022-04-13 12:19:23 +0200

    Merge branch 'main' of https://github.com/nsouff/CACAO2022 into main

--- src/abstraction/eq6Distributeur1/Distributeur1.java
+++ src/abstraction/eq6Distributeur1/Distributeur1.java
@@ -9,11 +9,66 @@ import java.util.Hashtable;
 public class Distributeur1 extends Distributeur1Acteur {
 	private double prixInstantane;
 	private double prixStockage;
-	private double prixStockageAactuel;
+	private double prixStockageActuel;
 	private Map<String, Double> teteGondole = new HashMap<String, Double>();
+	private int qteStockage;
+	private Map<String, Double> Stockage = new HashMap<String, Double>();
+	private Map<String, Integer> Achat = new HashMap<String, Integer>();
 
 	public Distributeur1() {
 		super();
+	}
+	
+//Emma Humeau a écrit ce code pour la gestion du stockage
+	
+	/**
+	 * @return the prixStockageActuel
+	 */
+	public double getPrixStockageActuel() {
+		return prixStockageActuel;
+	}
+
+	/**
+	 * @param prixStockageActuel the prixStockageActuel to set
+	 */
+	public void setPrixStockageActuel(double prixStockage, int qteStockage) {
+		this.prixStockageActuel = prixStockage*qteStockage;
+	}
+
+	/**
+	 * @return the qteStockage
+	 */
+	public int getQteStockage() {
+		return qteStockage;
+	}
+
+	/**
+	 * @param qteStockage the qteStockage to set
+	 */
+	
+	public void setQteStockage(int qteStockage, Map Achat) {
+		this.qteStockage = qteStockage ;
+	}
+
+	/**
+	 * @return the stockage
+	 */
+	public Map<String, Double> getStockage() {
+		return Stockage;
+	}
+
+	/**
+	 * @param stockage the stockage to set
+	 */
+	public void setStockage(Map<String, Double> stockage) {
+		Stockage = stockage;
+	}
+
+	/**
+	 * @return the prixStockage
+	 */
+	public double getPrixStockage() {
+		return prixStockage;
 	}
 		
 
