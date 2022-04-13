3f3606d55ca5433926d220ace39a2edcb47e834a
xGeaZx <yves.bernard999@outlook.fr>
2022-04-13 11:48:01 +0200

    Implements et exends sur Venteontrat

--- src/abstraction/eq5Transformateur3/VenteContrat.java
+++ src/abstraction/eq5Transformateur3/VenteContrat.java
@@ -1,5 +1,51 @@
 package abstraction.eq5Transformateur3;
 
-public class VenteContrat {
+import java.awt.Color;
+import java.util.List;
 
-}
+import abstraction.eq8Romu.contratsCadres.Echeancier;
+import abstraction.eq8Romu.contratsCadres.ExemplaireContratCadre;
+import abstraction.eq8Romu.contratsCadres.IVendeurContratCadre;
+import abstraction.eq8Romu.filiere.Filiere;
+import abstraction.eq8Romu.filiere.IActeur;
+import abstraction.eq8Romu.general.Journal;
+import abstraction.eq8Romu.general.Variable;
+
+public class VenteContrat extends Transformateur3Acteur implements IVendeurContratCadre {
+
+	@Override
+	public boolean vend(Object produit) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Echeancier contrePropositionDuVendeur(ExemplaireContratCadre contrat) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double propositionPrix(ExemplaireContratCadre contrat) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double contrePropositionPrixVendeur(ExemplaireContratCadre contrat) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void notificationNouveauContratCadre(ExemplaireContratCadre contrat) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public double livrer(Object produit, double quantite, ExemplaireContratCadre contrat) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	}
