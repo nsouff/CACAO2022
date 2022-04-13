dd684c47ba9ba07d0beb071c283f66fbc9919a90
maxim <maxim@172.16.122.162>
2022-04-13 11:35:57 +0200

    setters

--- src/abstraction/eq1Producteur1/arbre.java
+++ src/abstraction/eq1Producteur1/arbre.java
@@ -4,26 +4,31 @@ public class arbre {
 	private int ut_plantation;
 	private int ut_esperance_vie;
 	private int maladie;
-	private int qualité;
-	private boolean bioéquitable;
+	private int qualite ;
+	private boolean bioequitable;
 	private boolean transition_bio;
 	private int stade_transition;
 	
-	
-	public void set_ut_plantation(int ut_plantation) {
+	public void setUt_plantation(int ut_plantation) {
 		this.ut_plantation = ut_plantation;
 	}
-	
-	public void 
+	public void setUt_esperance_vie(int ut_esperance_vie) {
+		this.ut_esperance_vie=ut_esperance_vie;
+	}
 	public void setMaladie(int maladie) {
 		this.maladie=maladie;
 	}
-	public void setQualité() {
-		this.qualité=qualité;
-	
+	public void setQualite(int qualite) {
+		this.qualite=qualite;
 	}
-	public void setBioéquitable() {
-		this.bioéquitable= 
+	public void setBioéquitable(boolean bioequitable) {
+		this.bioequitable= bioequitable;
+	}
+	public void setTransition_bio(boolean transition_bio) {
+		this.transition_bio=transition_bio;
+	}
+	public void setStade_transition(int stade_transition) {
+		this.stade_transition = stade_transition;
 	}
 
 }
