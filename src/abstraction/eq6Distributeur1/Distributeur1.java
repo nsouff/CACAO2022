0632a792df2e223ab85ba9f707914f6ec6a1d97e
Nolann <Nolann@DESKTOP-628O5QI>
2022-04-13 11:13:44 +0200

    début variables dans Distributeur1 (= "noyau")

--- src/abstraction/eq6Distributeur1/Distributeur1.java
+++ src/abstraction/eq6Distributeur1/Distributeur1.java
@@ -1,10 +1,31 @@
 package abstraction.eq6Distributeur1;
 
 import java.util.Map;
+import java.util.Dictionary;
+import java.util.Enumeration;
+import java.util.Hashtable;
 
 public class Distributeur1 extends Distributeur1Acteur {
+	private double prix_instantane;
+	private double prix_stockage;
+	private double prix_stockage_actuel;
+	
 
 	public Distributeur1() {
 		super();
 	}
+		
+	historique_prix	
+	historique_demande	
+	historique_appel_offre	
+	appel_offre_instantane	
+	stock_instantane	
+	historique_stock	
+	prevision_stock	
+	historique_tete_gondole	
+	tete_gondole_instantané	
+		
+	prix_stock_actuel	
+	
+	
 }
