e5988d72f0819024abc72b4dfa34b46e79565812
mgloc <mgloc@172.16.122.83>
2022-04-13 11:51:57 +0200

    Interface IStock : adding seuil

--- src/abstraction/eq7Distributeur2/IStock.java
+++ src/abstraction/eq7Distributeur2/IStock.java
@@ -4,10 +4,18 @@ import abstraction.eq8Romu.produits.ChocolatDeMarque;
 
 public interface IStock {
 	
+	//Stocks
 	
 	public double getQuantite(ChocolatDeMarque chocolat);
 	
 	public double addProduit(ChocolatDeMarque chocolat, Double quantite);
 	
 	public void remove(ChocolatDeMarque chocolat,Double quantite);
+	
+	
+	//Seuil de rachat
+	
+	public double getSeuilRachat(ChocolatDeMarque chocolat);
+	
+	public double setSeuilRachat(ChocolatDeMarque chocolat, Double seuil);
 }
