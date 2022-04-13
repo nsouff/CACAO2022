e6cad2e8820fdd7f127be26d3cb4db1419416921
mgloc <mgloc@172.16.122.83>
2022-04-13 10:23:32 +0200

    Similar to last commit

---
+++ src/abstraction/eq7Distributeur2/Distributeur2ChocolatDeMarque.java
@@ -0,0 +1,124 @@
+package abstraction.eq7Distributeur2;
+
+import java.awt.Color;
+import java.util.List;
+
+import abstraction.eq8Romu.clients.ClientFinal;
+import abstraction.eq8Romu.filiere.Filiere;
+import abstraction.eq8Romu.filiere.IActeur;
+import abstraction.eq8Romu.filiere.IDistributeurChocolatDeMarque;
+import abstraction.eq8Romu.general.Journal;
+import abstraction.eq8Romu.general.Variable;
+import abstraction.eq8Romu.produits.ChocolatDeMarque;
+
+public class Distributeur2ChocolatDeMarque extends Distributeur2AchatContratCadre implements IDistributeurChocolatDeMarque {
+
+	@Override
+	public String getNom() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Color getColor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void initialiser() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void next() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<String> getNomsFilieresProposees() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Filiere getFiliere(String nom) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Variable> getIndicateurs() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Variable> getParametres() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Journal> getJournaux() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setCryptogramme(Integer crypto) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void notificationFaillite(IActeur acteur) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void notificationOperationBancaire(double montant) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public double prix(ChocolatDeMarque choco) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double quantiteEnVente(ChocolatDeMarque choco, int crypto) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double quantiteEnVenteTG(ChocolatDeMarque choco, int crypto) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void vendre(ClientFinal client, ChocolatDeMarque choco, double quantite, double montant, int crypto) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void notificationRayonVide(ChocolatDeMarque choco, int crypto) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
