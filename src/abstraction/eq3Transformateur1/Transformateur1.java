33b1dd42f3c212e469a1c26b0e597807d1e57952
akysh <akysh@172.16.125.93>
2022-04-13 11:50:05 +0200

    Transformation V1

--- src/abstraction/eq3Transformateur1/Transformateur1.java
+++ src/abstraction/eq3Transformateur1/Transformateur1.java
@@ -1,8 +1,34 @@
 package abstraction.eq3Transformateur1;
 
+import abstraction.eq8Romu.produits.Gamme;
+import abstraction.eq8Romu.produits.Chocolat;
+import java.util.HashMap;
+import abstraction.eq8Romu.produits.Feve;
+
 public class Transformateur1 extends Transformateur1Acteur {
+	private HashMap<Feve, Integer> stockFeve;
+	private HashMap<Chocolat,Integer> stockChoco;
 
 	public Transformateur1() { 
 		super();
+	}
+	
+	/** détermine la quantité à transformer */ 
+	public int transfoQt(int stockFeveQt) {
+		return stockFeveQt ;
+	}
+	
+	/** détermine le type de transformation */
+	public String transfoType(int contratCadre, Gamme stockFeveType) {
+		if (stockFeveType.equals(Gamme.BASSE)) {
+			return "transfoHaute" ;
+		} /** else if(stockFeveType.equals(Gamme.MOYENNE)){
+			
+		} */
+		return "transfoBasse";
+	}
+	
+	public Chocolat transfo() {
+		return null ;
 	}
 }
