193a47ded80a6a0a704c3ddc4172c8d7ea349685
annacharles <annacharles@dhcp-134-138.mde.emn.fr>
2022-04-13 11:50:16 +0200

    debut du code achat de fève

--- src/abstraction/eq3Transformateur1/Transformateur1.java
+++ src/abstraction/eq3Transformateur1/Transformateur1.java
@@ -1,8 +1,30 @@
 package abstraction.eq3Transformateur1;
+import java.util.List;
+
+
+
 
 public class Transformateur1 extends Transformateur1Acteur {
+	
+	private List<Double> dernierprixvente ;
+	private List<Double> prixtransfo ;
+	private List<Integer> stock;
+	private List<Integer> quantitévendue ;
 
+	
 	public Transformateur1() { 
 		super();
 	}
+	
+	public List<Double> prixmaxachat(List<Double> dernierprixvente ) {
+		List<Double> prixmaxachat = new ArrayList<Double>() ;
+		for (int i=0;i<this.dernierprixvente.size();i++) {
+			prixmaxachat.add(dernierprixvente.get(i) / (prixtransfo.get(i)*quantitévendue.get(i)*1.0));	
+		}
+		return prixmaxachat;
+	}
+	}
+	
+	
+	
 }
