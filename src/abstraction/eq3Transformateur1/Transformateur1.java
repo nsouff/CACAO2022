bd2b41879f3d5a29ae626990d49dc078e177bab8
akysh <akysh@LAPTOP-G21AK3FM.home>
2022-04-23 16:12:19 +0200

    Toutes les fonctions en lien avec la transformation

--- src/abstraction/eq3Transformateur1/Transformateur1.java
+++ src/abstraction/eq3Transformateur1/Transformateur1.java
@@ -7,57 +7,104 @@ import java.util.List;
 
 import abstraction.eq8Romu.produits.Gamme;
 import abstraction.eq8Romu.produits.Chocolat;
+import abstraction.eq8Romu.produits.ChocolatDeMarque;
+
 import java.util.HashMap;
 import abstraction.eq8Romu.produits.Feve;
 
 public class Transformateur1 extends Transformateur1Acteur {
-
-	private List<Double> dernierprixvente ;
+	public static final int rendementHaute=1;
+	public static final int coutTransfoNormal=1;
+	public static final int coutTransfoOriginal=2;
+	
 	private List<Double> prixtransfo ;
-	private List<Integer> stock;
-	private List<Integer> quantitévendue ;
-
-
-
+	private HashMap<Feve, Integer> quantiteAchat;
+	private HashMap<Chocolat, Integer> quantiteDemandee;
+	private HashMap<Chocolat, Integer> quantiteVendue;
+	private HashMap<Chocolat, Double> dernierPrixVente;
+	private HashMap<Feve, Double> prixAchat;
 	private HashMap<Feve, Integer> stockFeve;
 	private HashMap<Chocolat,Integer> stockChoco;
-
 	
-
 	public Transformateur1() { 
 		super();
 	}
 
+	/**
+	 * @return the stockFeve
+	 */
+	public HashMap<Feve, Integer> getStockFeve() {
+		return stockFeve;
+	}
 
-	public List<Double> prixmaxachat(List<Double> dernierprixvente ) {
-		List<Double> prixmaxachat = new ArrayList<Double>() ;
-		for (int i=0;i<this.dernierprixvente.size();i++) {
-			prixmaxachat.add(dernierprixvente.get(i) / (prixtransfo.get(i)*quantitévendue.get(i)*1.0));	
-		}
-		return prixmaxachat;
+	/** détermine le prix d'achat max; auteur Julien  */
+	public void prixMaxAchat() {		
+			prixAchat.put(Feve.FEVE_BASSE,dernierPrixVente.get(Chocolat.MQ) / prixtransfo.get(1));	
+			prixAchat.put(Feve.FEVE_MOYENNE,dernierPrixVente.get(Chocolat.MQ) / prixtransfo.get(0));
+			prixAchat.put(Feve.FEVE_MOYENNE_BIO_EQUITABLE,dernierPrixVente.get(Chocolat.MQ_BE) / prixtransfo.get(0));
 	}
 	
-	/** détermine la quantité à transformer */ 
+	/** détermine la quantité de fèves à acheter; auteur Julien */
+	public void determinationQuantiteAchat() {		
+		quantiteAchat.put(Feve.FEVE_BASSE,((quantiteDemandee.get(Chocolat.MQ)-stockChoco.get(Chocolat.MQ))/2));	
+		quantiteAchat.put(Feve.FEVE_MOYENNE,((quantiteDemandee.get(Chocolat.MQ)-stockChoco.get(Chocolat.MQ))/2));
+		quantiteAchat.put(Feve.FEVE_MOYENNE_BIO_EQUITABLE,(quantiteDemandee.get(Chocolat.MQ_BE)-stockChoco.get(Chocolat.MQ_BE)));
+	}
+
+	/**
+	 * @param stockChoco the stockChoco to set
+	 */
+	public void setStockChoco(HashMap<Chocolat, Integer> stockChoco) {
+		this.stockChoco = stockChoco;
+	}
+	
+	/** détermine la quantité à transformer 
+	 * Alexandre */ 
 	public int transfoQt(int stockFeveQt) {
 		return stockFeveQt ;
 	}
 	
-	/** détermine le type de transformation */
-	public String transfoType(int contratCadre, Gamme stockFeveType) {
+	/** détermine le type de transformation 
+	 * Alexandre */
+	public String choixTypeTransfo(Gamme stockFeveType) {
 		if (stockFeveType.equals(Gamme.BASSE)) {
 			return "transfoHaute" ;
-		} /** else if(stockFeveType.equals(Gamme.MOYENNE)){
+		} /** else if(stockFeveType.equals(Gamme.MOYENNE) // stockFeveType.equals(Gamme.MOYENNE)){
 			
 		} */
 		return "transfoBasse";
 	}
 	
-	public Chocolat transfo() {
-		return null ;
+	/** calcule le coût de la transformation et la quantité de chocolat produite
+	 * Alexandre */
+	public ArrayList<Integer> coutQuantiteTransfo(String typeTransfo, int quantiteFeve, boolean original) {
+		ArrayList<Integer> prixQuantite = new ArrayList<Integer>();
+		if (original) {
+			prixQuantite.add(quantiteFeve*coutTransfoOriginal);
+		}
+		prixQuantite.add(quantiteFeve*coutTransfoNormal);
+		if (typeTransfo.contentEquals("transfoHaute")) {
+			prixQuantite.add(quantiteFeve*rendementHaute);
+			return prixQuantite;
+		}
+		prixQuantite.add(quantiteFeve);
+		return prixQuantite;
 	}
-
+	
+	/** modifie les stocks suite à la transformation 
+	 * (penser à ajouter l'exception si quantitefeve>quantiteStockFeve) 
+	 * Alexandre */
+	public void transfo(int quantiteFeveTransformee, Feve feve, boolean original) {
+		for (Feve f : stockFeve.keySet()) {
+			if (f == feve) {
+				stockFeve.put(feve, stockFeve.get(feve)-quantiteFeveTransformee);
+			}
+		}
+		ArrayList<Integer> coutQuantiteTransfo = this.coutQuantiteTransfo(this.choixTypeTransfo(feve.getGamme()), quantiteFeveTransformee, original);
+		for (Chocolat c : stockChoco.keySet()) {
+			if (c.getGamme()==Gamme.MOYENNE && c.isBioEquitable()==feve.isBioEquitable() && c.isOriginal()==original) {
+				stockChoco.put(c, stockChoco.get(c)-coutQuantiteTransfo.get(1));
+			}
+		}
+	}
 }
-
-
-
-
