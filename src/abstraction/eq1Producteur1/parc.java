eeb7afaeb796aa589e8dc7e8aa729281f7d77441
Antoine <Antoine@172.17.2.234>
2022-04-27 09:39:06 +0200

    Création class Parc

---
+++ src/abstraction/eq1Producteur1/parc.java
@@ -0,0 +1,59 @@
+package abstraction.eq1Producteur1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class parc {
+	private ArrayList<arbre> cacaoyers;
+	private int nombre_BE;
+	private int nombre_non_BE;
+	
+	public parc() {
+		this.cacaoyers = new ArrayList<arbre>();
+		this.nombre_BE = 0;
+		this.nombre_non_BE = 0;
+	}
+	
+	public List<arbre> getCacaoyers() {
+		return this.cacaoyers;
+	}
+	
+	public int getNombre_BE() {
+		return this.nombre_BE;
+	}
+	
+	public int getNombre_non_BE() {
+		return this.nombre_non_BE;
+	}
+	
+	public void setNombre_BE(int i) {
+		this.nombre_BE = i;
+	}
+	
+	public void setNombre_non_BE(int i) {
+		this.nombre_non_BE = i;
+	}
+	
+	public arbre getArbre(int i) {
+		return this.getCacaoyers().get(i);
+	}
+	
+	public void Planter(arbre a) {
+			this.getCacaoyers().add(a);
+	}
+	
+	public void MAJParc() { 
+		for (int i=0; i<this.getCacaoyers().size(); i++) {
+			arbre arbre_i = this.getArbre(i);
+			if (arbre_i.getMaladie() == 5) {
+				this.getCacaoyers().remove(arbre_i);
+				if (arbre_i.getBioequitable()) {
+					nombre_BE--;
+				}
+				else {
+					nombre_non_BE--;
+				}
+			}
+		}
+	}
+}
