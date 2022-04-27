c037a59c73eada50c960601207acc69cf1b54310
Nolann <Nolann@DESKTOP-628O5QI>
2022-04-27 11:34:53 +0200

    idem

--- src/abstraction/eq6Distributeur1/Distributeur1.java
+++ src/abstraction/eq6Distributeur1/Distributeur1.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import abstraction.eq8Romu.produits.Chocolat;
+import abstraction.eq8Romu.produits.ChocolatDeMarque;
 
 import java.util.Dictionary;
 import java.util.Enumeration;
@@ -13,21 +13,18 @@ import java.util.Hashtable;
 
 public class Distributeur1 extends Distributeur1Acteur {
 
-	private Map<Chocolat,Double> prixInstantanneChoco = new HashMap<Chocolat, Double>();
+	private Map<ChocolatDeMarque,Double> prixInstantanneChoco = new HashMap<ChocolatDeMarque, Double>();
 
 	private double prixInstantane;
 	private double prixStockageActuel;
-	private Map<Chocolat, Double> teteGondole = new HashMap<Chocolat, Double>(); // (nom du chocolat,% en tête de gondole) 
+	private Map<ChocolatDeMarque, Double> teteGondole = new HashMap<ChocolatDeMarque, Double>(); // (nom du ChocolatDeMarque,% en tête de gondole) 
 	private int qteStockage;
-	private Map<Chocolat, ArrayList<Double>> Stockage = new HashMap<Chocolat, ArrayList<Double>>(); //(nom choco, table 2 entrées prix et qté)
+	private Map<ChocolatDeMarque, ArrayList<Double>> Stockage = new HashMap<ChocolatDeMarque, ArrayList<Double>>(); //(nom choco, table 2 entrées prix et qté)
 
-	private Map<Chocolat, Double> Stockage = new HashMap<Chocolat, Double>(); 
-	private Map<Chocolat, Integer> Achat = new HashMap<Chocolat, Integer>();
-<<<<<<< HEAD
-	
-=======
-	private Map<Chocolat, ArrayList<Double>> prixVente = new HashMap<Chocolat, ArrayList<Double>>();
->>>>>>> branch 'main' of https://github.com/nsouff/CACAO2022
+	private Map<ChocolatDeMarque, Double> Stockage = new HashMap<ChocolatDeMarque, Double>(); 
+	private Map<ChocolatDeMarque, Integer> Achat = new HashMap<ChocolatDeMarque, Integer>();
+
+	private Map<ChocolatDeMarque, ArrayList<Double>> prixVente = new HashMap<ChocolatDeMarque, ArrayList<Double>>();
 
 
 //github.com/nsouff/CACAO2022
@@ -37,39 +34,4 @@ public class Distributeur1 extends Distributeur1Acteur {
 	
 
 
-	/**
-	 * @return the qteStockage
-	 */
-	public int getQteStockage() {
-		return qteStockage;
-	}
-
-	/**
-	 * @param qteStockage the qteStockage to set
-	 */
-	
-	public void setQteStockage(int qteStockage, Map Achat) {
-		this.qteStockage = qteStockage ;
-	}
-
-	/**
-	 * @return the stockage
-	 */
-	public Double getStockage(Chocolat chocolat) {//leorouppert
-		return (this.Stockage).get(chocolat);
-	}
-
-	/**
-	 * @param stockage the stockage to set
-	 */
-	public void setStockage(Chocolat chocolat, Double stock) {//leorouppert
-		this.Stockage.put(chocolat, stock);
-	}
-
-	/**
-	 * @return the prixStockage
-	 */
-	public double getPrixStockage() {
-		return prixStockage;
-	}
 
