0340078dbd4534890a14cf0dbe67212ab1496452
Nathan Souffan <souffan.nathan@gmail.com>
2022-04-27 11:42:14 +0200

    Supprime données inutiles Distributeurs1

--- src/abstraction/eq6Distributeur1/Distributeur1.java
+++ src/abstraction/eq6Distributeur1/Distributeur1.java
@@ -1,28 +1,6 @@
 package abstraction.eq6Distributeur1;
-
-<<<<<<< HEAD
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
-import abstraction.eq8Romu.produits.ChocolatDeMarque;
-
-import java.util.Dictionary;
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.Hashtable;
-
 public class Distributeur1 extends Distributeur1Acteur {
-
-
-	
-
 	public Distributeur1() {
 		super();
 	}
-
-	
-
-
-
 }
