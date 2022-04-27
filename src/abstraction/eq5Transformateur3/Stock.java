593e1c04bf7229e133a084c5510887bf5a976595
karla <karla@dhcp-121-50.mde.emn.fr>
2022-04-27 08:58:11 +0200

    ajout de la methode getstock

--- src/abstraction/eq5Transformateur3/Stock.java
+++ src/abstraction/eq5Transformateur3/Stock.java
@@ -1,14 +1,19 @@
 package abstraction.eq5Transformateur3;
-import java.util.ArrayList;
 import java.util.HashMap;
 
-import abstraction.eq8Romu.produits.Feve;
+//karladedeler
 
 public class Stock<Produit> {
 	private HashMap <Produit, Double> stock;
-	private ArrayList <Feve> L;
 
 public void ajouter(Produit p, double qtt) {
-	this.stock.put(p, this.stock.get(p)+qtt);
+	if (qtt > 0) {
+		this.stock.put(p, this.stock.get(p)+qtt);
+	}
 }
+
+public Double getstock(Produit p) {
+	return this.stock.get(p);
+}
+
 }
