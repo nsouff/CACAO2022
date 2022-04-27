8c21407f0868532b9c6b0c3a898d0785cd731df4
pasqu <pasqu@DESKTOP-4UIUO7M.eduroam.emn.fr>
2022-04-27 08:42:54 +0200

    Stock

--- src/abstraction/eq5Transformateur3/Stock.java
+++ src/abstraction/eq5Transformateur3/Stock.java
@@ -1,9 +1,14 @@
 package abstraction.eq5Transformateur3;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import abstraction.eq8Romu.produits.Feve;
 
-public class Stock extends Transformateur3Acteur {
-	public HashMap <Feve, Double> StockFeves;
-	
+public class Stock<Produit> {
+	private HashMap <Produit, Double> stock;
+	private ArrayList <Feve> L;
+
+public void ajouter(Produit p, double qtt) {
+	this.stock.put(p, this.stock.get(p)+qtt);
+}
 }
