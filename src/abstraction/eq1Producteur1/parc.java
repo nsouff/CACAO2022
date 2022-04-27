d6b73a51522c7298a9e35b4da21011035a00245f
Antoine <Antoine@172.17.2.234>
2022-04-27 11:17:59 +0200

    Ajout auteur

--- src/abstraction/eq1Producteur1/parc.java
+++ src/abstraction/eq1Producteur1/parc.java
@@ -3,10 +3,16 @@ package abstraction.eq1Producteur1;
 import java.util.ArrayList;
 import java.util.List;
 
+import abstraction.eq8Romu.filiere.Filiere;
+
 public class parc {
 	private ArrayList<arbre> cacaoyers;
 	private int nombre_BE;
 	private int nombre_non_BE;
+	private boolean guerre;
+	private int ut_debut_guerre;
+	private int ut_fin_guerre;
+	private int duree_aleas;
 	
 	public parc() {
 		this.cacaoyers = new ArrayList<arbre>();
@@ -26,6 +32,22 @@ public class parc {
 		return this.nombre_non_BE;
 	}
 	
+	public boolean getGuerre() {
+		return this.guerre;
+	}
+	
+	public int getUt_debut_guerre() {
+		return this.ut_debut_guerre;
+	}
+	
+	public int getUt_fin_guerre() {
+		return this.ut_fin_guerre;
+	}
+	
+	public int getDuree_aleas() {
+		return this.duree_aleas;
+	}
+	
 	public void setNombre_BE(int i) {
 		this.nombre_BE = i;
 	}
@@ -34,12 +56,43 @@ public class parc {
 		this.nombre_non_BE = i;
 	}
 	
+	public void setGuerre(boolean b) {
+		this.guerre = b;
+	}
+	
+	public void setUt_debut_guerre(int ut) {
+		this.ut_debut_guerre = ut;
+	}
+	
+	public void setUt_fin_guerre(int ut) {
+		this.ut_fin_guerre = ut;
+	}
+	
+	public void setDuree_aleas(int i) {
+		this.duree_aleas = i;
+	}
 	public arbre getArbre(int i) {
 		return this.getCacaoyers().get(i);
 	}
 	
 	public void Planter(arbre a) {
 			this.getCacaoyers().add(a);
+	}
+	
+	public void MAJGuerre() {
+		if (Filiere.LA_FILIERE.getEtape()>this.getUt_fin_guerre()+Math.ceil((this.getUt_fin_guerre()-this.getUt_debut_guerre())*1.5)) {
+			double d = Math.random();
+			if (d<=0.15) {
+				this.setGuerre(true);
+				this.setUt_debut_guerre(Filiere.LA_FILIERE.getEtape());
+				double dd = Math.random();
+				while (dd==0) {
+					dd = Math.random();
+				}
+				int temps = (int)Math.ceil(dd*6);
+				this.setUt_fin_guerre(this.getUt_debut_guerre()+temps);
+			}
+		}
 	}
 	
 	public void MAJParc() { 
