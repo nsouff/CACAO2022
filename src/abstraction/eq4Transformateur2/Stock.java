61689581684cc929d5872d02ef2f870cd9c56c2e
marie <marie@172.16.125.37>
2022-04-27 10:15:54 +0200

    delete

---
+++ src/abstraction/eq4Transformateur2/Stock.java
@@ -0,0 +1,25 @@
+package abstraction.eq4Transformateur2;
+
+import java.util.HashMap;
+
+public class Stock<I> {
+		private HashMap<I,Double> quantite;
+		
+	public Stock() {
+		this.quantite = new HashMap<I, Double>();
+	}
+	public void ajouter (I produit, double qt) {
+		if (qt>0) {	
+			
+			if (this.quantite.keySet().contains(produit) ) {
+				this.quantite.put(produit, this.quantite.get(produit)+qt);
+			}else {
+				this.quantite.put(produit, qt);}
+	}else{
+		throw new 
+	}
+
+}
+
+
+}
--- src/abstraction/eq4Transformateur2/StockChoco.java
+++
@@ -1,5 +0,0 @@
-package abstraction.eq4Transformateur2;
-
-public class StockChoco extends Stock {
-
-}
