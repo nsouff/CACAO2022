0917714dd2fe247676afb323cfc34755caca94c9
Nolann <Nolann@DESKTOP-628O5QI>
2022-04-27 11:38:38 +0200

    12345

--- src/abstraction/eq6Distributeur1/Distributeur1.java
+++ src/abstraction/eq6Distributeur1/Distributeur1.java
@@ -12,27 +12,11 @@ import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Hashtable;
 
-=======
->>>>>>> branch 'main' of https://github.com/nsouff/CACAO2022
 public class Distributeur1 extends Distributeur1Acteur {
-<<<<<<< HEAD
-
-	private Map<ChocolatDeMarque,Double> prixInstantanneChoco = new HashMap<ChocolatDeMarque, Double>();
-
-	private double prixInstantane;
-	private double prixStockageActuel;
-	private Map<ChocolatDeMarque, Double> teteGondole = new HashMap<ChocolatDeMarque, Double>(); // (nom du ChocolatDeMarque,% en tête de gondole) 
-	private int qteStockage;
-	private Map<ChocolatDeMarque, ArrayList<Double>> Stockage = new HashMap<ChocolatDeMarque, ArrayList<Double>>(); //(nom choco, table 2 entrées prix et qté)
-
-	private Map<ChocolatDeMarque, Double> Stockage = new HashMap<ChocolatDeMarque, Double>(); 
-	private Map<ChocolatDeMarque, Integer> Achat = new HashMap<ChocolatDeMarque, Integer>();
-
-	private Map<ChocolatDeMarque, ArrayList<Double>> prixVente = new HashMap<ChocolatDeMarque, ArrayList<Double>>();
 
 
-=======
->>>>>>> branch 'main' of https://github.com/nsouff/CACAO2022
+	
+
 	public Distributeur1() {
 		super();
 	}
