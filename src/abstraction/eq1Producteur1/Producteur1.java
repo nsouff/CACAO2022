6f1554d5c1b797cf27dd23db60bceda62c639e23
Khéo <Kho@172.16.124.112>
2022-04-27 09:58:44 +0200

    Fonctionnement du comportement en bourse

--- src/abstraction/eq1Producteur1/Producteur1.java
+++ src/abstraction/eq1Producteur1/Producteur1.java
@@ -2,7 +2,7 @@ package abstraction.eq1Producteur1;
 
 import abstraction.eq8Romu.produits.Feve;
 
-public class Producteur1 extends Producteur1Acteur{
+public class Producteur1 extends ProducteurActeur1VenteBourse{
 
 	public Producteur1() {	
 		super(Feve.FEVE_BASSE, 10000);
--- src/abstraction/eq1Producteur1/ProducteurActeur1VenteBourse.java
+++ src/abstraction/eq1Producteur1/ProducteurActeur1VenteBourse.java
@@ -1,5 +1,8 @@
 package abstraction.eq1Producteur1;
 
+import java.util.HashMap;
+import java.util.List;
+
 import abstraction.eq8Romu.bourseCacao.BourseCacao;
 import abstraction.eq8Romu.bourseCacao.IVendeurBourse;
 import abstraction.eq8Romu.filiere.Filiere;
@@ -7,21 +10,47 @@ import abstraction.eq8Romu.general.Journal;
 import abstraction.eq8Romu.produits.Feve;
 
 public class ProducteurActeur1VenteBourse extends Producteur1Acteur implements IVendeurBourse{
+	private HashMap<Feve, Double> prixmoyenFeve ;
+	
+	
 	/**
 	 * @param feve
 	 * @param stock
 	 */
-	
 	public ProducteurActeur1VenteBourse(Feve feve, double stock) {
 		super(feve, stock);
-		// TODO Auto-generated constructor stub
+		this.prixmoyenFeve = new HashMap<Feve, Double>() ;
+		for (Feve f : Feve.values()){
+			this.getPrixmoyenFeve().put(f, 0.0); //On initialise la HashMap avec des prix à 0 
+		}
+
 	}
 
 
 
 	@Override
 	public double offre(Feve f, double cours) {
-		return this.stockFeve.getValeur();
+		//On met à jour les prix de la HashMap
+		this.getPrixmoyenFeve().put(f, this.getPrixmoyenFeve().get(f)+cours);
+		
+		
+		//On vends en fonction du prix
+		if (Filiere.LA_FILIERE.getEtape()>=1) {
+			if ((this.getPrixmoyenFeve().get(f)/Filiere.LA_FILIERE.getEtape()) < cours) {
+				return this.stockFeve.getValeur();
+			}
+		}
+		
+		return 0.0 ;
+	}
+
+
+
+	/**
+	 * @return the prixmoyenFeve
+	 */
+	public HashMap<Feve, Double> getPrixmoyenFeve() {
+		return prixmoyenFeve;
 	}
 
 
