2b7385883bf1c38264f58868e74ab7b4f671dbb6
TokiM4 <bacquaert.mathis@gmail.com>
2022-04-27 12:01:38 +0200

    modifs vente

--- src/abstraction/eq7Distributeur2/Distributeur2ChocolatDeMarque.java
+++ src/abstraction/eq7Distributeur2/Distributeur2ChocolatDeMarque.java
@@ -1,62 +1,73 @@
 package abstraction.eq7Distributeur2;
 
+import java.util.List;
+
 import abstraction.eq8Romu.clients.ClientFinal;
 import abstraction.eq8Romu.filiere.IDistributeurChocolatDeMarque;
 import abstraction.eq8Romu.produits.ChocolatDeMarque;
 
-public class Distributeur2ChocolatDeMarque extends Distributeur2Acteur implements IRayon  {
 
+public class Distributeur2ChocolatDeMarque extends Distributeur2Acteur implements IDistributeurChocolatDeMarque  {
+	private double capaciteDeVente;
+	private double[] prix;
 
-	public double getQuantiteClassique(ChocolatDeMarque chocolat) {
+	public double prix(ChocolatDeMarque choco) {
 		// TODO Auto-generated method stub
-		return this.stock.getQuantite(chocolat);
+		int pos= (chocolats.indexOf(choco));
+		if (pos<0) {
+			return 0.0;
+		} else {
+			return prix[pos];
+		}
 	}
 
-	@Override
-	public void rechargerClassique(ChocolatDeMarque chocolat, Double quantite) {
+	public double quantiteEnVente(ChocolatDeMarque choco, int crypto) {
 		// TODO Auto-generated method stub
+		if (crypto!=this.cryptogramme) {
+			journal.ajouter("Quelqu'un essaye de me pirater !");
+			return 0.0;
+		} else {
+			int pos= (chocolats.indexOf(choco));
+			if (pos<0) {
+				return 0.0;
+			} else {
+				return Math.min(capaciteDeVente, super.stock.getQuantite(choco));
+			}
+		}
+	}
+
+	public double quantiteEnVenteTG(ChocolatDeMarque choco, int crypto) {
+		// TODO Auto-generated method stub
+		if (crypto!=this.cryptogramme) {
+			journal.ajouter("Quelqu'un essaye de me pirater !");
+			return 0.0;
+		} else {
+			int pos= (chocolats.indexOf(choco));
+			if (pos<0) {
+				return 0.0;
+			} else {
+				return Math.min(capaciteDeVente, super.stock.getQuantite(choco))/10.0;
+			}
+		}
+	}
+
+	public void vendre(ClientFinal client, ChocolatDeMarque choco, double quantite, double montant, int crypto) {
+		// TODO Auto-generated method stub
+		int pos= (chocolats.indexOf(choco));
+		if (pos>=0) {
+			super.stock.remove(choco, quantite);
+		}
 		
 	}
 
-	@Override
-	public void venteClassique(ChocolatDeMarque chocolat, Double quantite) {
+	public void notificationRayonVide(ChocolatDeMarque choco, int crypto) {
 		// TODO Auto-generated method stub
-		
+		if (crypto!=this.cryptogramme) {
+			journal.ajouter("Quelqu'un essaye de me pirater !");
+		} else {
+			journal.ajouter("Je n'ai pas assez mis en vente de "+choco);
+		}
 	}
 
-	@Override
-	public void renvoyerEnStockClassique(ChocolatDeMarque chocolat, Double quantite) {
-		// TODO Auto-generated method stub
-		
-	}
 
-	@Override
-	public double getQuantiteGondole(ChocolatDeMarque chocolat) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public void rechargerGondole(ChocolatDeMarque chocolat, Double quantite) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void venteGondole(ChocolatDeMarque chocolat, Double quantite) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void renvoyerEnStockGondole(ChocolatDeMarque chocolat, Double quantite) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public double getQuantiteTotale(ChocolatDeMarque chocolat) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
 }
