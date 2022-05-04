01cdb6a6e85005d6a2136b26c591fdcef1a3025e
Nathan Souffan <souffan.nathan@gmail.com>
2022-05-04 09:12:13 +0200

    Enleve conflit Distributeur1Acteur

--- src/abstraction/eq6Distributeur1/Distributeur1.java
+++ src/abstraction/eq6Distributeur1/Distributeur1.java
@@ -1,19 +1,4 @@
 package abstraction.eq6Distributeur1;
-<<<<<<< HEAD
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
-import abstraction.eq8Romu.produits.ChocolatDeMarque;
-
-import java.util.Dictionary;
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.Hashtable;
-
-=======
->>>>>>> branch 'main' of https://github.com/nsouff/CACAO2022
 public class Distributeur1 extends Distributeur1Acteur {
 	public Distributeur1() {
 		super();
