f4fe780697d1554c5d99c6161d33aeda7e80e636
karla <karla@dhcp-121-50.mde.emn.fr>
2022-05-04 11:07:06 +0200

    Transformation classique et upgrade

--- src/abstraction/eq5Transformateur3/Transformateur3Acteur.java
+++ src/abstraction/eq5Transformateur3/Transformateur3Acteur.java
@@ -20,7 +20,7 @@ public class Transformateur3Acteur implements IActeur {
 	protected Variable seuilTransformation;
 	protected Variable rendement;
 	protected Variable coutTransformation;
-	protected Variable coutOrginial;
+	protected Variable coutOrignial;
 	protected Stock<Feve> stockFeves;
 	protected Stock<Chocolat> stockChocolat;
 
@@ -29,7 +29,7 @@ public class Transformateur3Acteur implements IActeur {
 		this.seuilTransformation = new VariableReadOnly ("seuiTransformation", "seuil de transformation par etape en tonne", this,  0, 100000, 100000);
 		this.rendement = new VariableReadOnly ("rendement", "rendement de la transformation longue", this,  0, 0.99, 0.7);
 		this.coutTransformation = new VariableReadOnly ("coutTransformation", "cout de transformation en milliers de dollars par etape par kg", this,  0, 1000, 5);
-		this.coutOrginial = new VariableReadOnly ("coutOrginial", "cout supplementaire pour un produire un chocolat orginal en milliers de dollars par etape par tonne", this, 0, 100, 1);
+		this.coutOrignial = new VariableReadOnly ("coutOrignial", "cout supplementaire pour un produire un chocolat orginal en milliers de dollars par etape par tonne", this, 0, 100, 1);
 		this.stockFeves = new Stock<Feve> ();
 		this.stockChocolat = new Stock<Chocolat> ();
 		this.seuilMaxAchat = 2900.00;
