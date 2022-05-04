730926f2dead1bd3eba89edee8b7d1c8f789c09e
gabyt <gabyt@172.16.125.246>
2022-05-04 12:16:41 +0200

    fin

--- src/abstraction/eq4Transformateur2/Transformateur2Achat.java
+++ src/abstraction/eq4Transformateur2/Transformateur2Achat.java
@@ -4,7 +4,7 @@ import abstraction.eq8Romu.filiere.Filiere;
 
 public class Transformateur2Achat extends Transformateur2Transfo{
 	
-<<<<<<< HEAD
+
 	public Transformateur2Achat(double stocktotalfeve, double stocktotalchoco) {
 		super(stocktotalfeve, stocktotalchoco);
 		// TODO Auto-generated constructor stub
@@ -12,18 +12,15 @@ public class Transformateur2Achat extends Transformateur2Transfo{
 
 	public double prix_seuil; // Prix d'achat maximum
 	private int stock_theorique; //On souhaite garder ce nombre de feves en stock constamment (opportunisme)
-=======
->>>>>>> branch 'main' of https://github.com/Gabeaugosse/CACAO2022.git
+
 	
 	public boolean comparer_prix(double prix_vente) {
-<<<<<<< HEAD
+
 		return prix_vente<prix_seuil; //On achete pas au dessus du prix seuil
 		
-=======
+
 		return prix_vente<super.getPrixSeuil().getValeur(); //On achete pas au dessus du prix seuil
->>>>>>> branch 'main' of https://github.com/Gabeaugosse/CACAO2022.git
 	}
-	
 	public double quantite(int stock) { 
 		return super.getCapaciteStockageFixe().getValeur()-super.getStockfeve().quantiteStockTotale();
 	}
