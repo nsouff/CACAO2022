93bdf4efd161de663dd145fe501769ed08eabd3f
annacharles <annacharles@dhcp-123-177.mde.emn.fr>
2022-05-04 12:07:18 +0200

    Merge branch 'main' of https://github.com/AnnaCharles/CACAO2022 into main

--- src/abstraction/eq3Transformateur1/Transformateur1AppelsOffres.java
+++ src/abstraction/eq3Transformateur1/Transformateur1AppelsOffres.java
@@ -8,43 +8,52 @@ import abstraction.eq8Romu.appelsOffres.SuperviseurVentesAO;
 import abstraction.eq8Romu.filiere.Filiere;
 import abstraction.eq8Romu.general.Variable;
 import abstraction.eq8Romu.produits.Chocolat;
+import abstraction.eq8Romu.produits.Feve;
 
-
+/**
 public class Transformateur1AppelsOffres extends Transformateur1ContratCadreAcheteur implements IVendeurAO {
 
-	protected Variable stock;
-	protected Chocolat choco;
+	
+
 	protected String marque;
 	protected double prixMin;
 	protected SuperviseurVentesAO superviseur;
 	protected double stockRestant;
 	
-	/** auteur Ilyas */
+	/** auteur Ilyas 
 	public void initialiser() {
 		this.superviseur = (SuperviseurVentesAO)(Filiere.LA_FILIERE.getActeur("Sup.AO"));}
 	
 	public void next() {
 		
 		if (Filiere.LA_FILIERE.getEtape()>=1) {
-			if (stockRestant != 0) {
-				PropositionAchatAO retenue = superviseur.vendreParAO(this, cryptogramme, getChocolatDeMarque(), stockRestant, false);
-				if (retenue!=null) {
-					this.stock.setValeur(this, this.stock.getValeur()-retenue.getOffre().getQuantiteKG());
-					
-				} else {
-					
+			for (Chocolat c : stockChoco.keySet()) {
+				stockRestant= stockChoco.get(c);
+			
+				if (stockRestant != 0) {
+					PropositionAchatAO retenue = superviseur.vendreParAO(this, cryptogramme, c, stockRestant, false);
+					if (retenue!=null) {
+						this.stockChoco.setValeur(this, this.stock.getValeur()-retenue.getOffre().getQuantiteKG());
+						
+					} else {
+						
+					}
 				}
 			}
 		}
 	}
 	
-	/** renvoie la meilleure proposition si celle-ci satisfait au vendeur; auteur Ilyas */
+	/** renvoie la meilleure proposition si celle-ci satisfait au vendeur; auteur Ilyas 
+	/** on veut faire par type de chocolat 
 	
 	public PropositionAchatAO choisir(List<PropositionAchatAO> propositions) {
 	
 		if (propositions==null) {
 			return null;
 		} else {
+			Chocolat c= new  Chocolat;
+			 Chocolat = propositions.get(0).getOffre().getChocolat();
+			if 	prixVenteMin.get(c)== propositions.getOffre().getChocolat() ;
 			PropositionAchatAO retenue = propositions.get(0);
 			if (retenue.getPrixKg()>this.prixMin) {
 				
@@ -58,4 +67,4 @@ public class Transformateur1AppelsOffres extends Transformateur1ContratCadreAche
   }
 		}
 
-
+*/
