d0015c0f9eba034935591928a4e564ad6e32b2c8
emmah78 <103105820+emmah78@users.noreply.github.com>
2022-05-04 10:14:26 +0200

    Merge branch 'main' of https://github.com/nsouff/CACAO2022 into main

--- src/abstraction/eq6Distributeur1/distributeurChocolatDeMarque.java
+++ src/abstraction/eq6Distributeur1/distributeurChocolatDeMarque.java
@@ -12,26 +12,26 @@ public class DistributeurChocolatDeMarque extends Distributeur1Acteur implements
 
 	private Map<ChocolatDeMarque, Double> teteGondole = new HashMap<ChocolatDeMarque, Double>(); // (nom du chocolat,% en tête de gondole), Emma Humeau 
 
-	
+	protected double qteEnVenteTG; //Emma Humeau
 	
 	/**
 	 * @return the teteGondole
 	 */
-	public Map<ChocolatDeMarque, Double> getTeteGondole() {
+	public Map<ChocolatDeMarque, Double> getTeteGondole() { //Emma Humeau
 		return teteGondole;
 	}
 
 	/**
 	 * @param teteGondole the teteGondole to set
 	 */
-	public void setTeteGondole(Map<ChocolatDeMarque, Double> teteGondole) {
+	public void setTeteGondole(Map<ChocolatDeMarque, Double> teteGondole, ChocolatDeMarque choco) { //Emma Humeau
+		teteGondole.put(choco, Math.random());
 		this.teteGondole = teteGondole;
 	}
 
 	@Override
-	public double prix(ChocolatDeMarque choco) {
-		// TODO Auto-generated method stub
-		return ;
+	public double prix(ChocolatDeMarque choco) { //Emma humeau
+		return prixVente.get(choco);
 	}
 
 	@Override
@@ -44,8 +44,20 @@ public class DistributeurChocolatDeMarque extends Distributeur1Acteur implements
 	}
 
 	@Override
-	public double quantiteEnVenteTG(ChocolatDeMarque choco, int crypto) {
-		return 0;
+	public double quantiteEnVenteTG(ChocolatDeMarque choco, int crypto) { //Emma humeau
+		//calcule la qte en vente en tete de gondole
+	
+				double sumqteTG = 0;
+						
+				for (Map.Entry<ChocolatDeMarque, Double > entry : teteGondole.entrySet()) {
+		           ChocolatDeMarque KeyQte = entry.getKey();
+		           Double ValueQte = entry.getValue();
+		           sumqteTG += ValueQte;
+				}
+				
+				this.qteEnVenteTG = sumqteTG;
+				
+		return qteEnVenteTG;
 	}
 
 	@Override
@@ -55,9 +67,13 @@ public class DistributeurChocolatDeMarque extends Distributeur1Acteur implements
 	}
 
 	@Override
-	public void notificationRayonVide(ChocolatDeMarque choco, int crypto) {
-		// TODO Auto-generated method stub
-		
+	public void notificationRayonVide(ChocolatDeMarque choco, int crypto) { //Emma Humeau
+		if  (teteGondole.isEmpty() == true ) {
+			System.out.println("Rayon vide pour la tête de gondole");
+		}
+		else {
+			System.out.println("Rayon pas vide");
+		}
 	}
 
 }
