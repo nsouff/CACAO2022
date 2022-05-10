8d36a3acd30acb7f109c33b618122c2c1af40bb7
karla <karla@dhcp-127-62.mde.emn.fr>
2022-05-10 21:01:53 +0200

    correction

--- src/abstraction/eq5Transformateur3/Transformateur3.java
+++ src/abstraction/eq5Transformateur3/Transformateur3.java
@@ -1,7 +1,6 @@
 package abstraction.eq5Transformateur3;
 
 import abstraction.eq8Romu.filiere.Filiere;
-import abstraction.eq8Romu.general.Variable;
 
 public class Transformateur3 extends VenteAppel {
 	
@@ -9,7 +8,8 @@ public class Transformateur3 extends VenteAppel {
 	public double prixStockage() {
 		double qttTotale = 0;    // on gère le chocolat et les fèves de la même manière pour les prix de stockage -> poids
 		qttTotale+= this.stockChocolat.getstocktotal()+this.stockFeves.getstocktotal() ;
-		return qttTotale*(((Variable) Filiere.LA_FILIERE.prixstockageVariable).getValeur()) ; }
+		return qttTotale*4*(Filiere.LA_FILIERE.getIndicateur("prixstockageVariable").getValeur()) ; 
+		}
 
 	public Transformateur3() {
 		super();
