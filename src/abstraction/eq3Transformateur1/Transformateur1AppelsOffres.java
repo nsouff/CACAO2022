8e99845db92a95e44114c9d971ff0a3b4af1f6e9
jbont <jbont@172.16.122.85>
2022-05-11 10:48:57 +0200

    appel offres Ilyas

--- src/abstraction/eq3Transformateur1/Transformateur1AppelsOffres.java
+++ src/abstraction/eq3Transformateur1/Transformateur1AppelsOffres.java
@@ -1,26 +1,30 @@
 package abstraction.eq3Transformateur1;
 
+import java.util.HashMap;
 import java.util.List;
 
 import abstraction.eq8Romu.appelsOffres.IVendeurAO;
 import abstraction.eq8Romu.appelsOffres.PropositionAchatAO;
 import abstraction.eq8Romu.appelsOffres.SuperviseurVentesAO;
 import abstraction.eq8Romu.filiere.Filiere;
-import abstraction.eq8Romu.general.Variable;
 import abstraction.eq8Romu.produits.Chocolat;
-import abstraction.eq8Romu.produits.Feve;
+
+import abstraction.eq8Romu.produits.ChocolatDeMarque;
 
 
-public class Transformateur1AppelsOffres extends Transformateur1ContratCadreAcheteur //implements IVendeurAO*/ {
-{/**
+
+
+public class Transformateur1AppelsOffres extends Transformateur1ContratCadreAcheteur implements IVendeurAO {
+
 	
 
 	protected String marque;
 	protected double prixMin;
 	protected SuperviseurVentesAO superviseur;
 	protected double stockRestant;
+	protected HashMap<Chocolat,Double> stockChoco;
 	
-	/** auteur Ilyas 
+	/** auteur Ilyas */
 	public void initialiser() {
 		this.superviseur = (SuperviseurVentesAO)(Filiere.LA_FILIERE.getActeur("Sup.AO"));}
 	
@@ -28,32 +32,29 @@ public class Transformateur1AppelsOffres extends Transformateur1ContratCadreAche
 		
 		if (Filiere.LA_FILIERE.getEtape()>=1) {
 			for (Chocolat c : stockChoco.keySet()) {
-				stockRestant= stockChoco.get(c);
-			
-				if (stockRestant != 0) {
-					PropositionAchatAO retenue = superviseur.vendreParAO(this, cryptogramme, c, stockRestant, false);
-					if (retenue!=null) {
-						this.stockChoco.setValeur(this, this.stock.getValeur()-retenue.getOffre().getQuantiteKG());
-						
-					} else {
-						
-					}
+
+				if (stockChoco.get(c)>=0) {
+					ChocolatDeMarque coco= new ChocolatDeMarque(c, "cote d'or");
+					double stock= stockChoco.get(c);
+					PropositionAchatAO retenue = superviseur.vendreParAO(this, cryptogramme, coco, stockChoco.get(c), false);
+					stockChoco.put(c, stock-retenue.getOffre().getQuantiteKG());
+				
+				
+					
 				}
+				
 			}
 		}
 	}
-	
+
 	/** renvoie la meilleure proposition si celle-ci satisfait au vendeur; auteur Ilyas 
-	/** on veut faire par type de chocolat 
-	
+	/** on veut faire par type de chocolat */
+
 	public PropositionAchatAO choisir(List<PropositionAchatAO> propositions) {
-	
+		
 		if (propositions==null) {
 			return null;
 		} else {
-			Chocolat c= new  Chocolat;
-			 Chocolat = propositions.get(0).getOffre().getChocolat();
-			if 	prixVenteMin.get(c)== propositions.getOffre().getChocolat() ;
 			PropositionAchatAO retenue = propositions.get(0);
 			if (retenue.getPrixKg()>this.prixMin) {
 				
@@ -63,9 +64,5 @@ public class Transformateur1AppelsOffres extends Transformateur1ContratCadreAche
 				return null;
 			}
 		}
-
-  }
+	}
 		}
-
-*/
-}
