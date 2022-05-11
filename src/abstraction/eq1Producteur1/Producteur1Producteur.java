854f0f14f19166cd913335cc510edfba7af0b77e
Antoine <Antoine@172.17.2.234>
2022-05-11 09:42:06 +0200

    Optimisation de l'initialisation (pas de calcul nombre_arbre_debut*pourcentage dans chaque if)

--- src/abstraction/eq1Producteur1/Producteur1Producteur.java
+++ src/abstraction/eq1Producteur1/Producteur1Producteur.java
@@ -2,6 +2,7 @@ package abstraction.eq1Producteur1;
 
 import java.util.HashMap;
 
+import abstraction.eq8Romu.general.Variable;
 import abstraction.eq8Romu.produits.Feve;
 
 public class Producteur1Producteur extends Producteur1Stock{
@@ -19,19 +20,31 @@ public class Producteur1Producteur extends Producteur1Stock{
 	public void initialiser() {
 		super.initialiser();
 		int nombre_arbre_debut = 500000;
+		double pourcentage_nBE_basse = 0.63;
+		double pourcentage_nBE_moyenne = 0.27;
+		double pourcentage_nBE_haute = 0;
+		double pourcentage_BE_moyenne = 0.05;
+		int nombre_arbre_nBE_basse = (int)Math.floor((pourcentage_nBE_basse*nombre_arbre_debut));
+		int nombre_arbre_nBE_moyenne = (int)Math.floor((pourcentage_nBE_basse+pourcentage_nBE_moyenne)*nombre_arbre_debut);
+		int nombre_arbre_nBE_haute = (int)Math.floor((pourcentage_nBE_basse+pourcentage_nBE_moyenne+pourcentage_nBE_haute)*nombre_arbre_debut);
+		int nombre_arbre_BE_moyenne = (int)Math.floor((pourcentage_nBE_basse+pourcentage_nBE_moyenne+pourcentage_nBE_haute+pourcentage_BE_moyenne)*nombre_arbre_debut);
+		int nombre_arbre_BE_haute= nombre_arbre_debut-(nombre_arbre_nBE_basse+nombre_arbre_nBE_moyenne+nombre_arbre_nBE_haute+nombre_arbre_BE_moyenne);
 		int ut_debut = -115;
 		for (int i=0;i<nombre_arbre_debut;i++) {
 			int d = (int)Math.random()*40;
-			if (i<nombre_arbre_debut*0.63) {
+			if (i<nombre_arbre_nBE_basse) {
 				this.getAfrique().Planter(new MilleArbre(1,false,ut_debut-d));
 			}
-			if ((i>=nombre_arbre_debut*0.63) && (i<nombre_arbre_debut*0.9)) {
+			if ((i>=nombre_arbre_nBE_basse) && (i<nombre_arbre_nBE_moyenne)) {
 				this.getAfrique().Planter(new MilleArbre(2,false,ut_debut-d));
 			}
-			if ((i>=nombre_arbre_debut*0.9) && (i<nombre_arbre_debut*0.95)) {
+			if ((i>=nombre_arbre_nBE_moyenne) && (i<nombre_arbre_nBE_haute)) {
+				this.getAfrique().Planter(new MilleArbre(3,false,ut_debut-d));
+			}
+			if ((i>=nombre_arbre_nBE_haute) && (i<nombre_arbre_BE_moyenne)) {
 				this.getAfrique().Planter(new MilleArbre(2,true,ut_debut-d));
 			}
-			if ((i>=nombre_arbre_debut*0.95)) {
+			if ((i>=nombre_arbre_BE_haute)) {
 				this.getAfrique().Planter(new MilleArbre(3,true,ut_debut-d));
 			}
 		}
