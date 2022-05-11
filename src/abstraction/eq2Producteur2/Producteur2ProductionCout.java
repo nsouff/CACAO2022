9dd98fb3e4214bdb34471c18ac2a62eb7e702d7f
m21saidi <mohamed.saidi2@imt-atlantique.net>
2022-05-11 09:30:57 +0200

    Merge branch 'main' of https://github.com/Clement-Charbonnier/CACAO2022 into main

--- src/abstraction/eq2Producteur2/Producteur2ProductionCout.java
+++ src/abstraction/eq2Producteur2/Producteur2ProductionCout.java
@@ -16,7 +16,7 @@ public class Producteur2ProductionCout extends Plantation {
 	public double coutHQ;
 	public double coutMQ;
 	public double coutBQ;
-	protected HashMap<Feve,Double> coutParKg;
+	public HashMap<Feve,Double> coutParKg;
 
 	public Producteur2ProductionCout() {
 		this.coutParKg.put(Feve.FEVE_HAUTE_BIO_EQUITABLE, (this.coutHQ_BE*this.getNbArbre(Feve.FEVE_HAUTE_BIO_EQUITABLE))/this.production(Feve.FEVE_HAUTE_BIO_EQUITABLE));
