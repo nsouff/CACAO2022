edd9efdce90c4c6ce3c24f6aca1b94e99fff08c5
Antoine <Antoine@172.17.2.234>
2022-05-18 11:08:56 +0200

    Modification journal RetourGuerre

--- src/abstraction/eq1Producteur1/Parc.java
+++ src/abstraction/eq1Producteur1/Parc.java
@@ -210,6 +210,10 @@ public class Parc {
 					this.getRetourGuerre().ajouter("Ces cancres de Producteur2 ne méritent pas qu'on leur déclare la guerre pour le moment");
 				}
 		}
+		if (	(Filiere.LA_FILIERE.getEtape()>=this.getUt_debut_guerre()) 
+				&& (Filiere.LA_FILIERE.getEtape()<this.getUt_fin_guerre())) {
+			this.getRetourGuerre().ajouter("Attends 2 secondes poupée, je finis de massacrer this.getProducteurNuls()");
+		}
 		if ((Filiere.LA_FILIERE.getEtape()<this.getUt_fin_guerre()+Math.ceil((this.getUt_fin_guerre()-this.getUt_debut_guerre())*1.5)) && (this.getGuerre()==false)) {
 			this.getRetourGuerre().ajouter("C'est la période de paix frérot, on cherche pas les embrouilles");
 		}
