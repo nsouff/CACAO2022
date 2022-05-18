8c0c4ad9979f3d0bac54570037562e68f4db6c82
laure <laure@172.16.134.171>
2022-05-18 11:18:40 +0200

    Creation classe transfo

---
+++ src/abstraction/eq1Producteur1/Producteur1Transfo.java
@@ -0,0 +1,16 @@
+package abstraction.eq1Producteur1;
+
+public class Producteur1Transfo extends Producteur1StockChocolat {
+	
+	
+
+	/**
+	 * @author laure
+	 * @param FeveProducteur1 feve : type de feve, double quantite : quantité à transformer
+	 * @return ChocolatProducteur1 : chocolat produit
+	 */
+	public void nextTransformation(FeveProducteur1 feve, double quantite) {
+		
+		
+	}
+}
