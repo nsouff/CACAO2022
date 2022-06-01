a472118e875b7ac6576a51d0fdf04f461efb0f2e
Keracles <keracles10@gmail.com>
2022-06-01 08:17:53 +0200

    Transformation chocolat

--- src/abstraction/eq1Producteur1/Producteur1Transfo.java
+++ src/abstraction/eq1Producteur1/Producteur1Transfo.java
@@ -19,10 +19,15 @@ public class Producteur1Transfo extends Producteur1StockChocolat {
 	 * @param FeveProducteur1 feve : type de feve, double quantite : quantité à transformer
 	 * @return ChocolatProducteur1 : chocolat produit
 	 */
-	public void nextTransformation(Feve f, double quantite, boolean original) {
+	public void nextTransformation(Feve f, boolean original) {
 		double quantiteChoco = this.getStock(f, false)*0.10; //On transforme 10% de notre quantité
 		
 		this.retirerQuantite(f, quantiteChoco);
 		this.addLot(Chocolat.get(f.getGamme(), f.isBioEquitable(), original), quantiteChoco);
+	}
+	
+	public void next() {
+		super.next();
+		this.nextTransformation(Feve.FEVE_BASSE, false);
 	}
 }
