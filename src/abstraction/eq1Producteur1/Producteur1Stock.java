83a2095fbd386a456578551ab787840bf4dc12d1
Keracles <keracles10@gmail.com>
2022-06-01 08:44:16 +0200

    Avoir la récolte en variable d'instance

--- src/abstraction/eq1Producteur1/Producteur1Stock.java
+++ src/abstraction/eq1Producteur1/Producteur1Stock.java
@@ -238,7 +238,7 @@ public class Producteur1Stock extends Producteur1Acteur {
 			
 			
 			if (Feve.isBioEquitable()) {
-				if (aleaParasiteGlobal <=0.25) { //Force du parasite
+				if (aleaParasiteGlobal <=1) { //Force du parasite
 					double aleaForceParasites = Math.random();
 					if (aleaForceParasites <= 0.70) {
 						// On perd 10% de notre stock
@@ -253,7 +253,7 @@ public class Producteur1Stock extends Producteur1Acteur {
 				}
 				
 			} else { // Pas bio-équitable moins de chance d'avoir un parasite
-				if (aleaParasiteGlobal <=0.15) {
+				if (aleaParasiteGlobal <=1) {
 					double aleaForceParasites = Math.random();
 					if (aleaForceParasites <= 0.70) {
 						// On perd 10% de notre stock
