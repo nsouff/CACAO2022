1f014c6fbfa429cec719adee1945445ce0e68dae
pasqu <pasqu@DESKTOP-4UIUO7M.eduroam.emn.fr>
2022-06-01 09:33:49 +0200

    fin peremption

--- src/abstraction/eq5Transformateur3/DateProd.java
+++ src/abstraction/eq5Transformateur3/DateProd.java
@@ -2,16 +2,16 @@ package abstraction.eq5Transformateur3;
 
 // julien
 public class DateProd<Produit>{
-	private Produit p;
-	private int date;
+	private Produit produit; 
+	private int date; // la date est en fait le step auquel le produit sera perdu 
 	
-	public DateProd(int date,Produit p) {
-		this.p=p;
+	public DateProd(int date,Produit produit) {
+		this.produit=produit;
 		this.date=date;
 	}
 	
 	public Produit getProduit() {
-		return this.p;
+		return this.produit;
 	}
 	public int getDate() {
 		return this.date;
