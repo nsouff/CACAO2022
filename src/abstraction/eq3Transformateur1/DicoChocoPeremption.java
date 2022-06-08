a538de91fb93a48d01c0feea0af4e940775ca316
jbont <jbont@172.16.122.85>
2022-06-08 11:41:59 +0200

    lot

--- src/abstraction/eq3Transformateur1/DicoChocoPeremption.java
+++ src/abstraction/eq3Transformateur1/DicoChocoPeremption.java
@@ -6,12 +6,12 @@ import java.util.List;
 
 import abstraction.eq8Romu.produits.Chocolat;
 /* anna*/
-public class DicoChocoPeremption extends HashMap<Chocolat, ArrayList<Lots> >{
+public class DicoChocoPeremption extends HashMap<Chocolat, ArrayList<Lot> >{
 	
 	
 	public DicoChocoPeremption() {
 		super();
-		ArrayList<Lots> listelots= new ArrayList<Lots>() ;
+		ArrayList<Lot> listelots= new ArrayList<Lot>() ;
 		for (Chocolat c : Chocolat.values()) {
 			this.put(c, listelots);
 		}
